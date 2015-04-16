8a9b8201a11f81ad8d2d973fd585f0a87769f6f2
Travis N. Thomsen <tthomsen@binaryvoid.com>
2015-04-16 17:19:59 -0500

    modify provision.sh to install Puppet 4

--- provision.sh
+++ provision.sh
@@ -8,29 +8,32 @@
 
 ###########################################################
 
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+#rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
 sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/puppetlabs.repo
 
 ## Add host entries for each system
 cat > /etc/hosts <<EOH
-127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain
-::1 localhost localhost.localdomain localhost6 localhost6.localdomain
 ###############################################################################
-192.168.1.25 master.os.vagrant.vm master
-192.168.1.26 agent1.os.vagrant.vm agent1
+192.168.1.25 master.os4.vagrant.vm master
+192.168.1.26 agent1.os4.vagrant.vm agent1
 
 ###############################################################################
 ## CNAMEs for CA server
-192.168.1.7 puppetca.vagrant.vm puppetca puppet.vagrant.vm puppet
-
+192.168.1.25 puppetca.vagrant.vm puppetca puppet.vagrant.vm puppet
 EOH
 
 
 service iptables stop
 ## Bootstrap the master(s)
 if [[ "$1" == master ]]; then
-  yum install puppet-server -y
   yum install puppetserver -y
+  echo "dns_alt_names = puppet,master,puppetca.vagrant.vm,puppetca,puppet.vagrant.vm" >> /etc/puppetlabs/puppet/puppet.conf
+  service puppetserver start
+else
+  yum install puppet-agent -y
+  echo "" >> /etc/puppetlabs/puppet/puppet.conf
+  echo "[agent]" >> /etc/puppetlabs/puppet/puppet.conf
+  echo "server =  master.os4.vagrant.vm" >> /etc/puppetlabs/puppet/puppet.conf
+  sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 fi
-
-yum install puppet -y
